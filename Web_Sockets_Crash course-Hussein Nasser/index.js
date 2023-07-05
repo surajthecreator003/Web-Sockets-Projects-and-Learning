72c1924c7065fe54f59df21303ba5fbd9cbaa2fc
Suraj Mallick <surajhustlermallick@gmail.com>
2023-07-06 01:19:11 +0530

    after writing a simple web sockeet servere that works with client

---
+++ Web_Sockets_Crash course-Hussein Nasser/index.js
@@ -0,0 +1,30 @@
+const http=require("http");
+const WebSocketServer=require("websocket").server
+
+const httpserver=http.createServer((req,res)=>{
+    console.log("we have received a request");
+})
+
+
+
+let connection=null;
+const websocket=new WebSocketServer({"httpServer":httpserver});//handshake part
+
+websocket.on("request",request=>{
+
+    connection=request.accept(null,request.origin);
+
+    connection.on("open",()=>{console.log("Open !")});
+
+    connection.on("close",()=>{console.log("Close !")});
+
+    
+    connection.on("message",message=>{
+        console.log(`Message Received from client ${message.utf8Data}`)
+    });
+})
+
+httpserver.listen(8081,()=>{
+    console.log("My server is listening on port 8081")
+})
+
